7b1707d34f1908a0b5771c1861129d9136e279d6
Miguel Casas-Sanchez <mcasas@chromium.org>
2017-05-18 17:20:24 -0700

    sorted out my silly mistakes in mediarecorder/demo.bundle.html

--- mediarecorder/demo.js
+++ mediarecorder/demo.js
@@ -17,39 +17,31 @@ async function reconstructIndex(blob) {
 // reconstructed Blob.
 // [1] https://github.com/legokichi/ts-ebml/issues/2#issuecomment-294293842
 function reconstructIndexOfArrayBuffer(webm_buf) {
-  const elms = new ebml.Decoder().decode(webm_buf);
-
-  let metadataElms = []; //: EBML.EBMLElementDetail[] = [];
   let metadataSize = 0;
   let last_duration = 0;
   const cluster_ptrs = []; //: number[] = [];
-  const reader = new ebml.EBMLReader();
-  reader.logging = true;
 
-  reader.addListener("metadata", ({data, metadataSize: size})=>{
-    metadataElms = data;
-    metadataSize = size;
-  });
+  const elms = new ebml.Decoder().decode(webm_buf);
 
-  reader.addListener("cluster_ptr", (ptr)=>{
-    cluster_ptrs.push(ptr);
-  });
+  const refined_elms =
+      ebml.tools.putRefinedMetaData(elms, cluster_ptrs, last_duration);
+  const refined_buf = new ebml.Encoder().encode(refined_elms);
 
-  reader.addListener("duration", ({timecodeScale, duration})=>{
-    last_duration = duration;
-  });
-
-  elms.forEach((elm)=>{ reader.read(elm); });
-  reader.stop();
-
-  const refinedMetadataElms =
-      tools.putRefinedMetaData(metadataElms, cluster_ptrs, last_duration);
-  const refinedMetadataBuf = new ebml.Encoder().encode(refinedMetadataElms);
-  const body = webm_buf.slice(metadataSize);
-
-  return new Blob([refinedMetadataBuf, body], {type: "video/webm"});
+  return new Blob([refined_buf, webm_buf], {type: "video/webm"});
 }
 
+
+function saveByteArray(data, name) {
+  var blob = new Blob(data, {type: "video/webm"});
+  var url = URL.createObjectURL(blob);
+  var a = document.createElement("a");
+  document.body.appendChild(a);
+  a.style = "display: none";
+  a.href = url;
+  a.download = name;
+  a.click();
+  URL.revokeObjectURL(url);
+}
 
 
 const width = 640;
@@ -75,10 +67,20 @@ function recorderOnDataAvailable(event) {
 }
 function recorderOnStop() {
   console.log('recorderOnStop fired');
+  var superBuffer = new Blob(recordedChunks);
+  reconstructIndex(superBuffer)
+      .then((reconstructed_blob) => {
+        console.log('reconstructed blob of size ' + reconstructed_blob.size);
+        saveByteArray(recordedChunks, 'test.webm');
+      })
+      .catch((e) => {
+        console.error(e.message);
+      });
 }
 function gotStreamFunction(stream) {
   console.log("getUserMedia succeeded :)");
   theStream = stream;
+  document.getElementById("video").src = URL.createObjectURL(stream);
   try {
     recorder = new MediaRecorder(stream);
   } catch (e) {
@@ -103,8 +105,5 @@ module.exports = {
 	stopRecording : function() {
 		theRecorder.stop();
 		theStream.getTracks().forEach((track) => { track.stop(); });
-
-	  var superBuffer = new Blob(recordedChunks);
-		var reconstructedBlob = reconstructIndex(superBuffer);
 	}
 }
